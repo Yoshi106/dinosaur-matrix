ffe0552972307c7ea29e209ced1990f76348839f
Yoshi106 <yshkbr@gmail.com>
2020-12-09 21:56:04 +0900

    Updated to return matrix correctly

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = function (width, height) {
-  return width * height;
+const makeMatrix = function (rows, columns, value) {
+  const element = value || 1;
+  let matrix = [];
+  for (let i = 0; i < rows; i++) {
+    let array = [];
+    for (let j = 0; j < columns; j++) {
+      array.push(element);
+    }
+    matrix[i] = array;
+  }
+  return matrix;
 };
+module.exports = makeMatrix;
